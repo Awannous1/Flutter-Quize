82ca0d027c3ea738ce3a2a05cf82b32dab601e9d
Ahmad wannous <ahmadwanous642@gmail.com>
2024-10-26 17:00:29 +0300

    Finished Screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,8 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Start Screen'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 70),
+          const Text(
+            'Learn Flutter THe Fun Way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 222, 197, 235),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
     );
   }
 }
